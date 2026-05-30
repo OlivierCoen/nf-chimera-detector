7d4532cff754dddc2e7dd3c0095b0e78687b5669
Olivier Coen <coen.olivier@gmail.com>
2026-05-30 09:00:27 +0200

    fix issue when merging chimeras

---
+++ bin/merge_chimeras.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# Written by Olivier Coen. Released under the MIT license.
+
+import argparse
+import logging
+from pathlib import Path
+
+import polars as pl
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+#####################################################
+#####################################################
+# FUNCTIONS
+#####################################################
+#####################################################
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Get chimera and non-chimera read coverage for each target"
+    )
+    parser.add_argument(
+        "--chimeras",
+        type=Path,
+        dest="chimeras_files",
+        nargs="+",
+        required=True,
+        help="Chimera table file",
+    )
+    parser.add_argument(
+        "--out",
+        type=Path,
+        dest="out_file",
+        required=True,
+        help="Output file path",
+    )
+    return parser.parse_args()
+
+
+def parse_chimeras_file(file: Path) -> pl.LazyFrame:
+    return pl.scan_csv(file, has_header=True)
+
+
+#####################################################
+#####################################################
+# MAIN
+#####################################################
+#####################################################
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # creating output file anyway
+    Path(args.out_file).touch()
+
+    with open(args.out_file, "a") as f:
+        for i, file in enumerate(args.chimeras_files):
+            chimeras_lf = parse_chimeras_file(file)
+            try:
+                if i == 0:
+                    chimeras_lf.sink_csv(f, include_header=True)
+                else:
+                    chimeras_lf.sink_csv(f, include_header=False)
+            except pl.exceptions.NoDataError:
+                logger.info(f"No data in file {file}")
+
+
+    logger.info("Done")
--- modules/local/merge_chimeras/main.nf
+++ modules/local/merge_chimeras/main.nf
@@ -1,6 +1,11 @@
 process MERGE_CHIMERAS {
     tag "${meta.id}"
 
+    conda "${moduleDir}/environment.yml"
+    container "${ workflow.containerEngine in ['singularity', 'apptainer'] && !task.ext.singularity_pull_docker_container ?
+        'https://community-cr-prod.seqera.io/docker/registry/v2/blobs/sha256/fd/fd1d206cd7587948d4761db523c452520f0e162468c9cf87055997e90c588fff/data':
+        'community.wave.seqera.io/library/polars:1.41.1--128accb4274f01d4' }"
+
     input:
     tuple val(meta), path(files, stageAs: "**/*")
 
@@ -10,7 +15,10 @@ process MERGE_CHIMERAS {
 
     script:
     """
-    cat ${files} > ${meta.id}.chimeras.csv
+    merge_chimeras.py \\
+        --chimeras ${files} \\
+        --out ${meta.id}.chimeras.csv
+
     NB_CHIMERAS=\$(cut -f 1 ${meta.id}.chimeras.csv | wc -l)
     """
 }

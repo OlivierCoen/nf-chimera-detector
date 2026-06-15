12279b3b53ef7d695c962f12839aa67c8d0327ab
Olivier Coen <coen.olivier@gmail.com>
2026-06-15 16:41:23 +0200

    Update merge_chimeras.py

--- bin/merge_chimeras.py
+++ bin/merge_chimeras.py
@@ -13,6 +13,49 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
+CHIMERA_COL_SCHEMA = pl.Schema(
+    {
+        "qseqid": pl.String(),
+        "sseqid_1": pl.String(),
+        "pident_1": pl.Float64(),
+        "length_1": pl.UInt32(),
+        "mismatch_1": pl.UInt32(),
+        "gapopen_1": pl.UInt32(),
+        "qstart_1": pl.UInt32(),
+        "qend_1": pl.UInt32(),
+        "sstart_1": pl.UInt64(),
+        "send_1": pl.UInt64(),
+        "qlen_1": pl.UInt32(),
+        "slen_1": pl.UInt64(),
+        "evalue_1": pl.Float64(),
+        "bitscore_1": pl.Float64(),
+        "sseqid_2": pl.String(),
+        "pident_2": pl.Float64(),
+        "length_2": pl.UInt32(),
+        "mismatch_2": pl.UInt32(),
+        "gapopen_2": pl.UInt32(),
+        "qstart_2": pl.UInt32(),
+        "qend_2": pl.UInt32(),
+        "sstart_2": pl.UInt64(),
+        "send_2": pl.UInt64(),
+        "qlen_2": pl.UInt32(),
+        "slen_2": pl.UInt64(),
+        "evalue_2": pl.Float64(),
+        "bitscore_2": pl.Float64(),
+        "total_coverage": pl.Int32(),
+        "overlap_length": pl.Int32(),
+        "coverage_1_only": pl.Int32(),
+        "coverage_2_only": pl.Int32(),
+        "chimeric": pl.Boolean(),
+        "coordinate_in_1": pl.Float64(),
+        "coordinate_in_2": pl.Float64(),
+        "family": pl.String(),
+        "species.taxid": pl.UInt64(),
+        "srr": pl.String(),
+    }
+)
+
 #####################################################
 #####################################################
 # FUNCTIONS
@@ -43,7 +86,7 @@ def parse_args():
 
 
 def parse_chimeras_file(file: Path) -> pl.LazyFrame:
-    return pl.scan_csv(file, has_header=True)
+    return pl.scan_csv(file, has_header=True, schema=CHIMERA_COL_SCHEMA)
 
 
 #####################################################
